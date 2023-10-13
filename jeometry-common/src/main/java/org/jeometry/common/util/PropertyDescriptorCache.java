3f829599749f5b2dfeae9ad2cfae6decd8f8ff40
Paul Austin <paul.austin@automutatio.com>
2023-10-13 15:02:15 -0700

    property descriptor cache

--- jeometry-common/src/main/java/org/jeometry/common/util/PropertyDescriptorCache.java
+++ jeometry-common/src/main/java/org/jeometry/common/util/PropertyDescriptorCache.java
@@ -1,29 +1,20 @@
 package org.jeometry.common.util;
 
-import java.beans.BeanInfo;
-import java.beans.IntrospectionException;
-import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Method;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.WeakHashMap;
 import java.util.concurrent.locks.ReentrantLock;
 
-import org.jeometry.common.collection.map.Maps;
-
 public class PropertyDescriptorCache {
-  private static Map<Class<?>, Map<String, PropertyDescriptor>> propertyDescriptorByClassAndName = new WeakHashMap<>();
-
-  private static Map<Class<?>, Map<String, Method>> propertyWriteMethodByClassAndName = new WeakHashMap<>();
+  private static Map<Class<?>, PropertyDescriptorCacheForClass> cacheForClass = new WeakHashMap<>();
 
   private static final ReentrantLock lock = new ReentrantLock();
 
   public static void clearCache() {
     lock.lock();
     try {
-      propertyDescriptorByClassAndName.clear();
-      propertyWriteMethodByClassAndName.clear();
+      cacheForClass.clear();
     } finally {
       lock.unlock();
     }
@@ -31,8 +22,8 @@ public class PropertyDescriptorCache {
 
   public static PropertyDescriptor getPropertyDescriptor(final Class<?> clazz,
     final String propertyName) {
-    final Map<String, PropertyDescriptor> propertyDescriptors = getPropertyDescriptors(clazz);
-    return propertyDescriptors.get(propertyName);
+    final var propertyDescriptors = getPropertyDescriptors(clazz);
+    return propertyDescriptors.getPropertyDescriptor(propertyName);
   }
 
   public static PropertyDescriptor getPropertyDescriptor(final Object object,
@@ -45,36 +36,13 @@ public class PropertyDescriptorCache {
     }
   }
 
-  protected static Map<String, PropertyDescriptor> getPropertyDescriptors(final Class<?> clazz) {
+  private static PropertyDescriptorCacheForClass getPropertyDescriptors(final Class<?> clazz) {
     lock.lock();
     try {
-      Map<String, PropertyDescriptor> propertyDescriptors = propertyDescriptorByClassAndName
-        .get(clazz);
+      var propertyDescriptors = cacheForClass.get(clazz);
       if (propertyDescriptors == null) {
-        propertyDescriptors = new HashMap<>();
-        try {
-          final BeanInfo beanInfo = Introspector.getBeanInfo(clazz);
-          for (final PropertyDescriptor propertyDescriptor : beanInfo.getPropertyDescriptors()) {
-            final String propertyName = propertyDescriptor.getName();
-            propertyDescriptors.put(propertyName, propertyDescriptor);
-            Method writeMethod = propertyDescriptor.getWriteMethod();
-            if (writeMethod == null) {
-              final String setMethodName = "set" + Character.toUpperCase(propertyName.charAt(0))
-                + propertyName.substring(1);
-              try {
-                final Class<?> propertyType = propertyDescriptor.getPropertyType();
-                writeMethod = clazz.getMethod(setMethodName, propertyType);
-                propertyDescriptor.setWriteMethod(writeMethod);
-              } catch (NoSuchMethodException | SecurityException e) {
-              }
-            }
-            Maps.put(propertyWriteMethodByClassAndName, clazz, propertyName, writeMethod);
-          }
-        } catch (final IntrospectionException e) {
-          // TODO Auto-generated catch block
-          e.printStackTrace();
-        }
-        propertyDescriptorByClassAndName.put(clazz, propertyDescriptors);
+        propertyDescriptors = new PropertyDescriptorCacheForClass(clazz);
+        cacheForClass.put(clazz, propertyDescriptors);
       }
       return propertyDescriptors;
     } finally {
@@ -83,17 +51,8 @@ public class PropertyDescriptorCache {
   }
 
   protected static Map<String, Method> getWriteMethods(final Class<?> clazz) {
-    lock.lock();
-    try {
-      Map<String, Method> writeMethods = propertyWriteMethodByClassAndName.get(clazz);
-      if (writeMethods == null) {
-        getPropertyDescriptors(clazz);
-        writeMethods = propertyWriteMethodByClassAndName.get(clazz);
-      }
-      return writeMethods;
-    } finally {
-      lock.unlock();
-    }
+    final var propertyDescriptors = getPropertyDescriptors(clazz);
+    return propertyDescriptors.getWriteMethods();
   }
 
 }
